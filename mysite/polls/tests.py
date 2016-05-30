782e9c78f0b3ba22aec0fb3f974684529a44219c
Calla <maracondo@aol.com>
2016-05-30 10:22:13 -0400

    Hi

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -6,6 +6,7 @@ from django.test import TestCase
 from .models import Question
 
 def QuestionMethodTests(TestCase):
+	
 	def test_was_published_recently_with_future_question(self):
 		time = timezone.now() + datetime.timedelta(days = 30)
 		future_question = Question(pub_date = time)
